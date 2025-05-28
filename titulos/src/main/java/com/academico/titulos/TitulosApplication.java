9648c64929db56127a00943255ecb39234543149
cesar9898 <graficacevallos@gmail.com>
2025-05-28 02:19:46 +0000

    cambio1

--- titulos/src/main/java/com/academico/titulos/TitulosApplication.java
+++ titulos/src/main/java/com/academico/titulos/TitulosApplication.java
@@ -1,21 +1,8 @@
 package com.academico.titulos;
 
-<<<<<<< HEAD
 
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-@SpringBootApplication
-public class TitulosApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(TitulosApplication.class, args);
-    }
-
-}
-=======
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -27,4 +14,4 @@ public class TitulosApplication {
 	}
 
 }
->>>>>>> ea92841ad6fac247b13b60b94d812f2db1c4ec81
+
